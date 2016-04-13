f27a39044c9991245c099924140885ba6ec4ed80
wxmimperio <wxmimperio@outlook.com>
2016-04-13 21:41:37 +0800

    "update file_test.py"

--- learning-python/learning_file/file_test.py
+++ learning-python/learning_file/file_test.py
@@ -12,7 +12,26 @@ file_read = open(file_path, 'r+')
 print file_read.read()
 file_read.close()
 
+"""
 # 写文件
 file_wtite = open(file_path, 'a+')
-file_wtite.write("This is a test!".decode("utf-8"))
+file_wtite.write("\nThis is a test!".decode("utf-8"))
 file_wtite.close()
+"""
+
+# 读一行数据
+file_readline = open(file_path, 'r+')
+print file_readline.readline().decode("utf-8")
+# 读指定行数
+lines = file_readline.readlines(2)
+for line in lines:
+	print line
+file_readline.close()
+
+"""
+# 写入一个字符串列表
+str_list = ["a\n", "b\n", "c\n"]
+file_writelines = open(file_path, 'a+')
+file_writelines.writelines(str_list)
+file_writelines.close()
+"""
